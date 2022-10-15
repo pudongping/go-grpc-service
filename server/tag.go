a2e729b6f1cc310a6c2d4a8b98a06a09a852abab
pudongping <276558492@qq.com>
2022-10-16 00:23:06 +0800

    提供数据服务

---
+++ server/tag.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/pudongping/go-grpc-service/pkg/bapi"
+	"github.com/pudongping/go-grpc-service/pkg/errcode"
+	pb "github.com/pudongping/go-grpc-service/proto"
+)
+
+type TagServer struct {
+}
+
+func NewTagServer() *TagServer {
+	return &TagServer{}
+}
+
+func (t *TagServer) GetTagList(ctx context.Context, r *pb.GetTagListRequest) (*pb.GetTagListResponse, error) {
+	api := bapi.NewAPI("http://127.0.0.1:8000")
+	body, err := api.GetTagList(ctx, r.GetName())
+	if err != nil {
+		return nil, errcode.TogRPCError(errcode.ErrorGetTagListFail)
+	}
+
+	tagList := pb.GetTagListResponse{}
+	err = json.Unmarshal(body, &tagList)
+	if err != nil {
+		return nil, errcode.TogRPCError(errcode.Fail)
+	}
+
+	return &tagList, nil
+}
