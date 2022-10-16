c5f71feed2c0b5d1e149602fb77c4c00895a1b78
pudongping <276558492@qq.com>
2022-10-16 15:01:25 +0800

    在一个应用程序中用不同端口监听 grpc 和 http 服务

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 
 	pb "github.com/pudongping/go-grpc-service/proto"
 	"github.com/pudongping/go-grpc-service/server"
@@ -11,24 +13,63 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
+var grpcPort string
+var httpPort string
+
+func init() {
+	flag.StringVar(&grpcPort, "grpc_port", "8001", "gRPC 启动端口号")
+	flag.StringVar(&httpPort, "http_port", "9001", "HTTP 启动端口号")
+	flag.Parse()
+}
+
 func main() {
+	errs := make(chan error)
+
+	go func() {
+		err := RunHttpServer(httpPort)
+		if err != nil {
+			errs <- err
+		}
+	}()
+
+	go func() {
+		err := RunGrpcServer(grpcPort)
+		if err != nil {
+			errs <- err
+		}
+	}()
+
+	select {
+	case err := <-errs:
+		log.Fatalf("Run Server err: %v", err)
+	}
+}
+
+func RunHttpServer(port string) error {
+	serveMux := http.NewServeMux()
+	serveMux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte(`pong`))
+	})
+
+	log.Printf("http server is started at: 127.0.0.1:%s \n", port)
+	return http.ListenAndServe(":"+port, serveMux)
+}
+
+func RunGrpcServer(port string) error {
 	s := grpc.NewServer()
 
 	pb.RegisterTagServiceServer(s, server.NewTagServer())
-
-	port := 5200
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
-	log.Printf("grpc server is started at: 127.0.0.1:%d", port)
-	if err != nil {
-		log.Fatalf("net.Listen err: %v", err)
-	}
 
 	// 注册反射服务，方便让 grpcurl 或者 grpcui 用作调试
 	// reflection 包是 gRPC 官方所提供的反射服务
 	reflection.Register(s)
 
-	err = s.Serve(lis)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
+	log.Printf("grpc server is started at: 127.0.0.1:%s \n", port)
 	if err != nil {
-		log.Fatalf("server.Serve err: %v", err)
+		log.Fatalf("net.Listen err: %v", err)
+		return err
 	}
+
+	return s.Serve(lis)
 }
